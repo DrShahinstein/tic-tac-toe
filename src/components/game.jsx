f66d651602926b2150f64697797b9d5a66eeaba0
1TaylanOzturk <taylanozturk.freedom@gmail.com>
2021-08-12 16:20:36 +0300

    rework

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -4,29 +4,30 @@ import Spot from "./spot";
 
 function Game(props) {
   // Functions
-  const handleClick = (e) => {
-    const spotID = e.target.getAttribute("id");
-    const indexes = coordination[spotID];
+  const handleClick = (event) => {
+    const targetSpot = event.target;
+    const targetSpotID = targetSpot.getAttribute("id");
+    const indexesOfTargetSpot = coordination[targetSpotID];
 
     if (settings.gameMode === "PvC") {
-      // If game mode is "Player vs Computer" ...
+      targetSpot.innerText = players.user;
+      updateBoardState(...indexesOfTargetSpot, players.user);
 
-      e.target.innerText = players.user; // Place the mark of user to the target spot having in DOM
-
-      // Place the mark of user to the target spot of `board` array/state.
-      updateBoardState(indexes[0], indexes[1]);
-
-      if (returnValueOfPosition(board) || !getDepth(board)) {
-        // Did game finish ? In this case:
-        setIsResultModalVisible(!isResultModalVisible);
+      if (!getDepth()) {
+        setShowResultModal(!showResultModal);
       } else {
-        // otherwise:
-        const bestValues = max(board);
-        updateBoardState(bestValues.row, bestValues.col, players.opponnent);
-        forceUpdate(); // Forcing React to re-render
+        const bestValuesForComputer = max();
+        updateBoardState(
+          bestValuesForComputer.row,
+          bestValuesForComputer.col,
+          players.opponnent
+        );
+
+        if (isFinished()) {
+          setShowResultModal(!showResultModal);
+        }
       }
     } else {
-      // Otherwise, if the game mode is PvP:
       let countOfX = 0;
       let countOfO = 0;
       let turn;
@@ -40,70 +41,57 @@ function Game(props) {
 
       if (!countOfX && !countOfO) {
         turn = "X";
+      } else if (countOfX && !countOfO) {
+        turn = "O";
       } else if (countOfX === countOfO) {
         turn = "X";
       } else if (countOfX > countOfO) {
         turn = "O";
+      } else if (countOfO > countOfX) {
+        turn = "X";
       }
 
-      e.target.innerText = turn; // Place the mark of user to the target spot having in DOM
-
-      // Place the mark of user to the target spot of `board` array/state.
-      updateBoardState(indexes[0], indexes[1], turn);
-
-      if (returnValueOfPosition(board) || !getDepth(board)) {
-        // Did game finish ? In this case:
-        setIsResultModalVisible(!isResultModalVisible);
+      updateBoardState(...indexesOfTargetSpot, turn);
+      if (isFinished()) {
+        setShowResultModal(!showResultModal);
       }
     }
   };
 
-  const playAgain = () => {
-    setIsResultModalVisible(!isResultModalVisible);
-    setBoard(initialBoard);
-  };
+  const min = () => {
+    const valueOfPosition = returnValueOfPosition();
 
-  // used for updating the `board` array easily.
-  const updateBoardState = (row, col, player = players.user) => {
-    let newBoard = board;
-    newBoard[row][col] = player;
-    setBoard(newBoard);
-  };
-
-  const min = (board) => {
-    const valueOfPosition = returnValueOfPosition(board);
-
-    if (valueOfPosition !== 0 || getDepth(board) === 0) {
+    if (isFinished()) {
       return valueOfPosition;
     }
 
-    let highestValue = Infinity;
+    let maxValue = Infinity;
 
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         if (board[i][j] === EMPTY) {
           board[i][j] = players.user;
-          const bestValuesForComputer = max(board);
+          const bestValuesForComputer = max();
 
-          if (bestValuesForComputer["point"] < highestValue) {
-            highestValue = bestValuesForComputer.point;
+          if (bestValuesForComputer.point < maxValue) {
+            maxValue = bestValuesForComputer.point;
           }
           board[i][j] = EMPTY;
         }
       }
     }
 
-    return highestValue;
+    return maxValue;
   };
 
-  const max = (board) => {
-    const valueOfPosition = returnValueOfPosition(board);
+  const max = () => {
+    const valueOfPosition = returnValueOfPosition();
 
-    if (valueOfPosition !== 0 || getDepth(board) === 0) {
+    if (isFinished()) {
       return { point: valueOfPosition, row: -1, col: -1 };
     }
 
-    let highestValue = -Infinity;
+    let lowestValue = -Infinity;
     let bestRow = undefined;
     let bestCol = undefined;
 
@@ -111,10 +99,10 @@ function Game(props) {
       for (let j = 0; j < 3; j++) {
         if (board[i][j] === EMPTY) {
           board[i][j] = players.opponnent;
-          const bestPointForUser = min(board);
+          const bestPointForUser = min();
 
-          if (bestPointForUser > highestValue) {
-            highestValue = bestPointForUser;
+          if (bestPointForUser > lowestValue) {
+            lowestValue = bestPointForUser;
             bestRow = i;
             bestCol = j;
           }
@@ -123,70 +111,92 @@ function Game(props) {
       }
     }
     return {
-      point: highestValue,
+      point: lowestValue,
       row: bestRow,
       col: bestCol,
     };
   };
 
-  const returnValueOfPosition = (position) => {
+  const returnValueOfPosition = () => {
     for (let i = 0; i < 3; i++) {
       if (
-        position[0][i] === position[1][i] &&
-        position[1][i] === position[2][i] &&
-        position[0][i] !== ""
+        board[0][i] === board[1][i] &&
+        board[1][i] === board[2][i] &&
+        board[0][i] !== EMPTY
       ) {
-        if (position[0][i] === players.user) return -1;
+        if (board[0][i] === players.user) return -1;
         else return 1;
       }
     }
 
     for (let i = 0; i < 3; i++) {
       if (
-        position[i][0] === position[i][1] &&
-        position[i][1] === position[i][2] &&
-        position[i][0] !== ""
+        board[i][0] === board[i][1] &&
+        board[i][1] === board[i][2] &&
+        board[i][0] !== EMPTY
       ) {
-        if (position[i][0] === players.user) return -1;
+        if (board[i][0] === players.user) return -1;
         else return 1;
       }
     }
 
     if (
-      position[0][0] === position[1][1] &&
-      position[1][1] === position[2][2] &&
-      position[0][0] !== ""
+      board[0][0] === board[1][1] &&
+      board[1][1] === board[2][2] &&
+      board[0][0] !== EMPTY
     ) {
-      if (position[0][0] === players.user) return -1;
+      if (board[0][0] === players.user) return -1;
       else return 1;
     }
 
     if (
-      position[0][2] === position[1][1] &&
-      position[1][1] === position[2][0] &&
-      position[0][2] !== ""
+      board[0][2] === board[1][1] &&
+      board[1][1] === board[2][0] &&
+      board[0][2] !== EMPTY
     ) {
-      if (position[0][2] === players.user) return -1;
+      if (board[0][2] === players.user) return -1;
       else return 1;
     }
     return 0;
   };
 
-  const getDepth = (position) => {
+  const updateBoardState = (row, col, player) => {
+    let currentBoard = board;
+    currentBoard[row][col] = player;
+    setBoard(currentBoard);
+    forceUpdate();
+  };
+
+  const isFinished = () => {
+    if ((!getDepth() && !returnValueOfPosition()) || returnValueOfPosition()) {
+      return true;
+    }
+    return false;
+  };
+
+  const getDepth = () => {
     let count = 0;
+
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (position[i][j] === "") {
-          count += 1;
-        }
+        if (board[i][j] === EMPTY) count += 1;
       }
     }
+
     return count;
+  };
+
+  const playAgain = () => {
+    setShowResultModal(!showResultModal);
+    setBoard(initialBoard);
+  };
+
+  const closeWarningModal = () => {
+    setShowWarningModal(!showWarningModal);
   };
 
   // Others (variables, states, JSX ...)
   const EMPTY = "";
-
   const initialBoard = [
     [EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY],
@@ -194,24 +204,20 @@ function Game(props) {
   ];
 
   const [board, setBoard] = useState(initialBoard);
-
-  const [isResultModalVisible, setIsResultModalVisible] = useState(false);
-  const [isWarningModalVisible, setIsWarningModalVisible] = useState(false);
+  const [showResultModal, setShowResultModal] = useState(false);
+  const [showWarningModal, setShowWarningModal] = useState(false);
   const forceUpdate = React.useReducer(() => ({}))[1];
 
   const settings = props.settings;
-
   const players = {
     user: settings.user,
     opponnent: settings.user === "X" ? "O" : "X",
   };
-
   let modalDatas = {
     title: undefined,
     bodyText: undefined,
   };
-
-  let coordination = {
+  const coordination = {
     1: [0, 0],
     2: [0, 1],
     3: [0, 2],
@@ -223,15 +229,15 @@ function Game(props) {
     9: [2, 2],
   };
 
-  if (isResultModalVisible) {
-    let finalPoint = max(board).point;
+  if (showResultModal) {
+    let finalPoint = returnValueOfPosition();
     if (finalPoint === 0) {
       modalDatas.title = "Draw";
       modalDatas.bodyText = "Match Has Ended with Draw !";
     }
     if (finalPoint === 1) {
-      modalDatas.title = "Computer";
-      modalDatas.bodyText = "Computer Has Won !";
+      modalDatas.title = "Opponnent";
+      modalDatas.bodyText = "Opponnent Has Won !";
     }
     if (finalPoint === -1) {
       modalDatas.title = "User";
@@ -241,43 +247,33 @@ function Game(props) {
 
   return (
     <>
-      <Modal show={isResultModalVisible}>
+      <Modal show={showResultModal}>
         <Modal.Header>
           <Modal.Title>{modalDatas.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>
-            {modalDatas.bodyText} <br />
-          </p>
+          <p>{modalDatas.bodyText}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={playAgain}>
-            Play Again
+          <Button variant="secondary" onClick={() => window.location.reload()}>
+            Go back to Settings
           </Button>
-          <Button variant="primary" onClick={() => window.location.reload()}>
-            Go Back to Settings
-          </Button>
+          <Button onClick={playAgain}>Play Again</Button>
         </Modal.Footer>
       </Modal>
-      <Modal show={isWarningModalVisible}>
+
+      <Modal show={showWarningModal}>
         <Modal.Header>
-          <Modal.Title>Error</Modal.Title>
+          <Modal.Title>You Cannot Move to a Filled Spot</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>You cannot move to a filled spot</p>
+          <p>You've tried to move to a filled spot but you it's illegal.</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button
-            onClick={() => setIsWarningModalVisible(!isWarningModalVisible)}
-            variant="secondary"
-          >
+          <Button variant="secondary" onClick={closeWarningModal}>
             Close
           </Button>
-          <Button
-            onClick={() => setIsWarningModalVisible(!isWarningModalVisible)}
-          >
-            OK
-          </Button>
+          <Button onClick={showWarningModal}>OK</Button>
         </Modal.Footer>
       </Modal>
 
